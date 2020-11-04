611961e86f9195d3969a5f47e95703a2df9a8df5
Ricardo Martins <martinsc.ricardo@gmail.com>
2020-11-04 15:40:57 -0300

    Criando projeto integrador MatchPet

---
+++ ProjetoIntegrador/src/test/java/com/petdevs/ProjetoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
