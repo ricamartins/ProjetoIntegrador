8503b71eaca35d290521d43fbacdd640fa032333
unknown <matheus.torok98@gmail.com>
2020-11-05 15:15:16 -0300

    Criação das models

--- ProjetoIntegrador/src/main/java/com/petdevs/models/Tema.java
+++ ProjetoIntegrador/src/main/java/com/petdevs/models/Tema.java
@@ -1,5 +1,39 @@
 package com.petdevs.models;
 
-public class Tema {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column
+	@NotNull
+	private String nomeTema;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeTema() {
+		return nomeTema;
+	}
+
+	public void setNomeTema(String nomeTema) {
+		this.nomeTema = nomeTema;
+	}
+
+	
 }
