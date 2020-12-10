ddfb106ca27bde66f6c0232af85f884ff9c54c86
Matheus Araujo <Matheus.moraaes@hotmail.com>
2020-12-10 09:07:55 -0300

    atualização na pagina sobre nos

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
   {path: "", component: IndexComponent},
   {path: "login", component: LoginComponent},
-  {path: "cadastro", component: CadastroComponent}
+  {path: "cadastro", component: CadastroComponent},
+  {path: "sobre", component: SobreNosComponent}
 
 ];
 
