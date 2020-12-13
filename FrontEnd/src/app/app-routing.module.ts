a84d31a1d7bfc6547b7f1f995f54a8444242e896
Phelipe-Souza <phelipe691@gmail.com>
2020-12-12 23:28:52 -0300

    Navbar e Fale Conosco

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { FaleConoscoComponent } from './fale-conosco/fale-conosco.component';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
   {path: "", component: IndexComponent},
   {path: "login", component: LoginComponent},
-  {path: "cadastro", component: CadastroComponent}
+  {path: "cadastro", component: CadastroComponent},
+  {path: "sobre-nos", component: SobreNosComponent},
+  {path: "contato", component: FaleConoscoComponent}
 
 ];
 
---
+++ FrontEnd/src/app/fale-conosco/fale-conosco.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fale-conosco',
+  templateUrl: './fale-conosco.component.html',
+  styleUrls: ['./fale-conosco.component.css']
+})
+export class FaleConoscoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
