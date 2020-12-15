c249a6dc1225b445f2baac59407ddf5281504b42
unknown <matheus.torok98@gmail.com>
2020-12-14 21:44:53 -0300

    não sei pq pediu pra eu comitar, ignore isso kk

--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  Usuario: Usuario = new Usuario();
+  usuario: Usuario = new Usuario();
   
   constructor() { }
 
