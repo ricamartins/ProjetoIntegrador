7fd8b339ab1d906a68474f36e14853e16da2407a
Phelipe-Souza <phelipe691@gmail.com>
2020-12-15 12:14:23 -0300

    Ajuste dos cards sobre-nós

--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  usuario: Usuario = new Usuario();
+  Usuario: Usuario = new Usuario();
   
   constructor() { }
 
